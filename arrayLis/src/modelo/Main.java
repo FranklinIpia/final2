c0ba851c68336d36754adeaee15eb8d15e5a06b0
Franklin Ipia <Franklin Ipia@172.16.1.117>
2018-11-13 15:34:46 -0500

    esta

---
+++ arrayLis/src/modelo/Main.java
@@ -0,0 +1,24 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	
+	ArrayList <Persona> persona =new ArrayList<Persona>();
+	
+	
+
+	public static void main(String[] args) {
+		String nombre;
+		
+		Scanner teclado = new Scanner (System.in);
+		
+		
+		System.out.println("Nombre");
+		nombre= teclado.nextLine();
+		
+
+	}
+
+}
